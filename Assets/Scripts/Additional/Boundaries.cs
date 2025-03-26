7dcd27fe177854897c811fcf43a1494b3d50aaa0
BekenAidana <bekenaidanaaskar@gmail.com>
2025-03-26 15:25:03 -0600

    fix: player rb movement at boundary
    Fix: Resolved issue with freezing/unfreezing positions when player enters or exits boundaries

--- Assets/Scripts/Additional/Boundaries.cs
+++ Assets/Scripts/Additional/Boundaries.cs
@@ -7,6 +7,7 @@ public class Boundaries : MonoBehaviour
     Rigidbody2D rb;
     Collider2D col;
     float halfWidth, halfHeight;
+    string gameObjectName;
     float repelForce = 5f;
     float minX=-15f, maxX=15f;
     float minY=-9f, maxY=9f;
@@ -15,6 +16,7 @@ public class Boundaries : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        gameObjectName = gameObject.name;
         halfWidth = col.bounds.extents.x;
         halfHeight = col.bounds.extents.y;
     }
@@ -24,7 +26,9 @@ public class Boundaries : MonoBehaviour
 
         maxX = LevelManager.instance.MaxX;
         Vector2 force = Vector2.zero;
-
+        
+        bool unconstrainX = false;
+        bool unconstrainY = false;
         float halfWidth = col.bounds.extents.x;
         float halfHeight = col.bounds.extents.y;
 
@@ -34,29 +38,45 @@ public class Boundaries : MonoBehaviour
         {
             force += Vector2.right * repelForce;
             isOutOfBounds = true;
+            unconstrainX = true;
         }
         else if (transform.position.x + halfWidth >= maxX)
         {
             force += Vector2.left * repelForce;
             isOutOfBounds = true;
+            unconstrainX = true;
         }
 
         if (transform.position.y - halfHeight <= minY)
         {
             force += Vector2.up * repelForce;
             isOutOfBounds = true;
+            unconstrainY = true;
         }
         else if (transform.position.y + halfHeight >= maxY)
         {
             force += Vector2.down * repelForce;
             isOutOfBounds = true;
+            unconstrainY = true;
         }
 
         rb.AddForce(force, ForceMode2D.Force);
 
-        // Останавливаем объект, если он уже внутри границ
+        if(unconstrainX && gameObjectName=="Player")
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+        }
+        if(unconstrainY && gameObjectName=="Player")
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;;
+        }
         if (!isOutOfBounds)
         {
+            if(gameObjectName=="Player")
+            {
+                Debug.Log("lol");
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            }
             rb.velocity = Vector2.zero;
         }
     }
